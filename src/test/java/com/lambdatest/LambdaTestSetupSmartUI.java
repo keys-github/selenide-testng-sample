643d2b7ef249e28181627bc4369fb724ad9f82c8
shreyanshc <shreyanshc@lambdatest.com>
2025-11-11 15:09:25 +0530

    updated LT capabilies to support w3c standards

--- src/test/java/com/lambdatest/LambdaTestSetupSmartUI.java
+++ src/test/java/com/lambdatest/LambdaTestSetupSmartUI.java
@@ -5,11 +5,10 @@ import java.net.URL;
 import java.time.Duration;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -32,29 +31,38 @@ public class LambdaTestSetupSmartUI {
 		JSONObject config = (JSONObject) parser.parse(new FileReader("src/test/resources/conf/" + config_file));
 		JSONObject envs = (JSONObject) config.get("environments");
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
+        MutableCapabilities capabilities = new MutableCapabilities();
+        MutableCapabilities ltOptions = new MutableCapabilities();
 
-		Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
-		Iterator it = envCapabilities.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pair = (Map.Entry) it.next();
-			capabilities.setCapability(pair.getKey().toString(), pair.getValue().toString());
-		}
+        Map<String, String> envCapabilities = (Map<String, String>) envs.get(environment);
+        Iterator it = envCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            ltOptions.setCapability(pair.getKey().toString(), pair.getValue().toString());
+        }
 
-		Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
-		it = commonCapabilities.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry pair = (Map.Entry) it.next();
-			if (capabilities.getCapability(pair.getKey().toString()) == null) {
-				capabilities.setCapability(pair.getKey().toString(),
-						(pair.getValue().toString().equalsIgnoreCase("true")
-								|| (pair.getValue().toString().equalsIgnoreCase("false"))
-										? Boolean.parseBoolean(pair.getValue().toString())
-										: pair.getValue().toString()));
-			}
-		}
-		capabilities.setCapability("name", this.getClass().getName());
-        capabilities.setCapability("smartUI.project","Selenide-Java");
+        Map<String, String> commonCapabilities = (Map<String, String>) config.get("capabilities");
+        it = commonCapabilities.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
+            if (ltOptions.getCapability(pair.getKey().toString()) == null) {
+                Object value = pair.getValue();
+                if (value.toString().equalsIgnoreCase("true") || value.toString().equalsIgnoreCase("false")) {
+                    ltOptions.setCapability(pair.getKey().toString(), Boolean.parseBoolean(value.toString()));
+                } else {
+                    ltOptions.setCapability(pair.getKey().toString(), value.toString());
+                }
+            }
+        }
+
+        ltOptions.setCapability("name", this.getClass().getName());
+        ltOptions.setCapability("smartUI.project", "Selenide-Java");
+
+        if (envCapabilities.containsKey("browserName")) {
+            capabilities.setCapability("browserName", envCapabilities.get("browserName"));
+        }
+
+        capabilities.setCapability("LT:Options", ltOptions);
 
 		username = System.getenv("LT_USERNAME");
 		if (username == null) {
